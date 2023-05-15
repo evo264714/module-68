c11645da1bbbac85f998db6a458e78602234aa81
evo264714 <mh264714@gmail.com>
2023-05-15 21:49:51 +0600

    done

---
+++ coffee-store-client/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { useLoaderData } from 'react-router-dom'
+import CoffeeCard from './components/CoffeeCard'
+import { useState } from 'react';
+
+function App() {
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees)
+  return (
+    <div className='m-20'>
+
+      <h1 className='text-center text-6xl text-purple-600 my-20'>Hot Coffees: {coffees.length}</h1>
+      <div className='grid md:grid-cols-2 gap-4 '>
+        {
+          coffees.map(coffee => <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+          ></CoffeeCard>)
+        }
+      </div>
+
+    </div>
+  )
+}
+
+export default App
